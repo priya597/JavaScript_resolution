1d7d1995ea44c3be6b908cc668a481da11b22dc2
priyag597 <priya.g@purplle.com>
2023-11-13 01:10:41 +0530

    bubble sort

---
+++ bubblesort.js
@@ -0,0 +1,18 @@
+function BubbleSort(arr){
+    do { 
+        isswap = false
+        for (let i = 0; i < arr.length - 1;i++ ){
+            if(arr[i] > arr[i+1]){
+                let temp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = temp
+                isswap = true
+            }
+        }
+    } while(isswap)
+}
+  
+let arr = [8, 20, -2, 4, -6, 0, -1.5]
+BubbleSort(arr)
+console.log(arr)
+
